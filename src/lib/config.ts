13b32811ed3c88595a9c17ced009967e88e47f3e
nilton <devplusis@gmail.com>
2025-11-26 16:41:49 -0500

    purebas para instalar dependencias

--- src/lib/config.ts
+++ src/lib/config.ts
@@ -2,6 +2,5 @@
 export const config = {
     API_URL: import.meta.env.VITE_API_URL,
     API_URL_DASH: import.meta.env.VITE_API_URL_DASH
-    // APP_NAME: import.meta.env.VITE_APP_NAME,
-    // DEBUG: import.meta.env.DEV,
+
 }
